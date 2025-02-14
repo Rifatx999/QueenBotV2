5165e253465d313b3d035039c2a4ff730c3a440f
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-02-14 06:57:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 /**
  * @author NTKhang
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
+ * ! The source code is written by NTKhang, please don't change the author's name everywhere.
  * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
- * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
- *
- * English:
- * ! Please do not change the below code, it is very important for the project.
- * It is my motivation to maintain and develop the project for free.
- * ! If you change it, you will be banned forever
- * Thank you for using
- *
- * Vietnamese:
- * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
- * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
- * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
- * Cảm ơn bạn đã sử dụng
+ * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked.
  */
 
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
+const express = require("express");
 
 function startProject() {
 	const child = spawn("node", ["Goat.js"], {
@@ -28,11 +17,22 @@ function startProject() {
 	});
 
 	child.on("close", (code) => {
-		if (code == 2) {
+		if (code === 2) {
 			log.info("Restarting Project...");
 			startProject();
 		}
 	});
 }
 
+// Start the bot
 startProject();
+
+// Create a dummy web server to prevent Render from shutting it down
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+	res.send("Goat Bot V2 is running!");
+});
+
+app.listen(PORT, () => console.log(`Web service running on port ${PORT}`));
